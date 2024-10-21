571cc81bbb39911886ee45eed78a2d9a5af07849
Lukas <lukas.schlewitz@gmail.com>
2024-10-21 17:32:29 +0200

    2023 01 1

---
+++ Vorlage/AoCFile.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+
+namespace Vorlage;
+
+public class AoCFile
+{
+    private ArrayList alFile;
+    
+    public AoCFile()
+    {
+        alFile = new ArrayList();
+        alFile = readFile("Input.txt");
+    }
+
+    public AoCFile(string path)
+    {
+        alFile = new ArrayList();
+        alFile = readFile(path);
+    }
+
+    private ArrayList readFile(string path)
+    {
+        ArrayList al = new ArrayList();
+        String line;
+
+        StreamReader sr = new StreamReader(path);
+        line = sr.ReadLine();
+        while (line != null)
+        {
+            al.Add(line);
+            line = sr.ReadLine();
+        }
+        sr.Close();
+
+        return al;
+    }
+    
+    public ArrayList getAoCFile() { return alFile; }
+}
---
+++ Vorlage/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Vorlage;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Program p = new Program();
+    }
+
+    public Program()
+    {
+        AoCFile af = new AoCFile();
+    }
+}
