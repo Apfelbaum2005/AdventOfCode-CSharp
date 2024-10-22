235aa946cbd793402193232c0ea89510414e98eb
Lukas <125089860+Apfelbaum2005@users.noreply.github.com>
2024-10-22 17:29:43 +0200

    2023 Initial

---
+++ AoC_2023/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Program program = new Program();
+    }
+
+    public Program()
+    {
+        string path = "C:\\Users\\Lukas\\RiderProjects\\AdventOfCode\\AoC_2023\\Input\\2023_01_1.txt";
+        AoCFile.AoCFile aocFile = new AoCFile.AoCFile(path);
+    }
+}
